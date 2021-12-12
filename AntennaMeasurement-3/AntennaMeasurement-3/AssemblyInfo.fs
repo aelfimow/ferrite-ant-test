f31001d48fb43a9264de170e0d0f190b75c68971
Alexander Elfimow <my.free.mail@gmx.de>
2021-12-12 20:47:18 +0100

    AntennaMeasurement-3: added file

---
+++ AntennaMeasurement-3/AntennaMeasurement-3/AssemblyInfo.fs
@@ -0,0 +1,23 @@
+﻿namespace AntennaMeasurement.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("AntennaMeasurement")>]
+[<assembly: AssemblyDescription("Various antenna measurement data")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("AntennaMeasurement")>]
+[<assembly: AssemblyCopyright("Copyright by Alexander Elfimow © 2018")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("a64e6de4-3d54-4217-b840-1dae875a5668")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
